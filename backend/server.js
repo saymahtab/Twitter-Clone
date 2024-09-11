3fd5982530503d8967124c290fb5c24c12fce830
saymahtab <reachmahtabsce@gmail.com>
2024-09-12 01:56:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const { postRoutes } = require('./routes/post.route');
 const { notificationRoutes } = require('./routes/notification.route');
 
 const { connectMongoDB } = require('./db/connectMongoDB');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -22,9 +23,15 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
