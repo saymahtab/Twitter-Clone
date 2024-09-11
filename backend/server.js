5b956f81b46621df7172a817379617ba520ca959
saymahtab <reachmahtabsce@gmail.com>
2024-09-12 01:41:25 +0530

    error solved

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,13 @@ app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
+      res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+    });
+  }
+  
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
